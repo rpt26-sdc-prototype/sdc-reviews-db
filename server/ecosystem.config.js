a37f0da72570cb802dd77408894c9cd0abea5238
James Hrivnak <jameshrivnak4@gmail.com>
2021-06-11 17:50:30 -0600

    much struggle leading to pm2 deployment

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
   }],
   deploy: {
     production: {
-      user: 'database',
+      user: 'ubuntu',
       host: 'ec2-3-140-90-28.us-east-2.compute.amazonaws.com',
       key: './sdc-db.pem',
       ref: 'origin/main',
-      repo: 'git@github.com:rpt26-sdc-prototype/sdc-reviews-db.git',
+      repo: 'https://github.com/rpt26-sdc-prototype/sdc-reviews-db.git',
       path: '/home/ubuntu/sdc-db',
       'post-deploy': 'npm install && pm2 startOrRestart ./server/ecosystem.config.js'
     }
